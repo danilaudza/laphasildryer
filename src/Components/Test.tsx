1636b1bbadd8ee98f5fb58847c1a8295c6e4f88a
Dhany Laudza <67975936+danilaudza@users.noreply.github.com>
2023-03-23 19:59:06 +0700

    new actions

--- src/Components/Test.tsx
+++ src/Components/Test.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import Select from "react-select";
 import { v4 as uuidv4 } from "uuid";
 import { optGrade, optJenis, optTebal, optUkuran } from "../Object/Options";
